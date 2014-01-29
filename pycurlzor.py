2249539b161e1d215aaa97c9aaf6027c19eef693
Zach Orr <zachorr139@gmail.com>
2014-01-29 09:29:39 -0500

    Removed old file (with now revoked auth token)

--- pycurlzor.py
+++
@@ -1,8 +0,0 @@
-import pycurl
-import json
-
-conn = pycurl.Curl()
-c.setopt(pycurl.URL, "https://api.github.com/gists")
-c.setopt(pycurl.HTTPHEADER, {'Authorization': bytes('token 2583d226c3ce002513aa5a29d0b87a15e6dfa46b', 'UTF-8'), 'User-Agent': bytes('ecxinc', 'UTF-8'), 'Content-Type': bytes('application/json', 'UTF-8')})
-c.setopt(pycurl.POST, 1)
-c.setopt(pycurl.POSTFIELDS, json.dumps(data))
