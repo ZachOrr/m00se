cf0861d5ad7d6b0908623b184180465ee41de9eb
Zach Orr <zachorr139@gmail.com>
2014-02-10 00:17:36 -0500

    or not ||

--- m00se.py
+++ m00se.py
@@ -129,7 +129,7 @@ class Moose(object):
 				break
 		if not r:
 			r = requests.post("https://api.github.com/gists", headers=self.headers, data=dumps(gist))
-		if r.status_code != 201 || r.status_code != 200:
+		if r.status_code != 201 or r.status_code != 200:
 			raise GistException("Couldn't create gist!")
 		return loads(r.text)["html_url"]
 
