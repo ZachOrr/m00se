99ace60bb03b1fc4c1a09a15f4f7a69d9f0c3b13
Zach Orr <zachorr139@gmail.com>
2014-02-18 14:38:26 -0500

    Added some output to redux

--- commands/redux.py
+++ commands/redux.py
@@ -3,5 +3,6 @@ from decorators import command
 @command("redux", argc=0, text="!redux - Reload commands")
 def redux(moose):
 	moose.redux()
+	moose.send_message("Commands reloaded!")
 
 # vim: noet:nosta
